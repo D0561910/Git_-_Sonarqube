dc7c08de3a0bc9e69d75512be05ea75808727110
d0561910 <d0561910@mail.fcu.edu.tw>
2020-06-05 09:15:29 +0800

    Update MainTest.java
    git update test.java

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -16,82 +16,50 @@ import org.junit.runners.JUnit4;
 public class MainTest {
 
     @Test
-	public void testNextDate01() {
-		assertEquals("month: 2 day: 3", Main.nextDate(2, 2));
+	public void testCheckTriangle1() {
+		Main test = new Main();
+		assertEquals("是直角三角形", test.checkTriangle(3, 4, 5));
 	}
 
 	@Test
-	public void testNextDate02() {
-		assertEquals("month: 3 day: 1", Main.nextDate(2, 28));
+	public void testCheckTriangle2() {
+		Main test = new Main();
+		assertEquals("三条边不能构成三角形", test.checkTriangle(-3, -4, -5));
 	}
 
 	@Test
-	public void testNextDate03() {
-		assertEquals("month: 3 day: 29", Main.nextDate(3, 28));
+	public void testCheckTriangle3() {
+		Main test = new Main();
+		assertEquals("三条边不能构成三角形", test.checkTriangle(-5, 12, -13));
 	}
 
 	@Test
-	public void testNextDate04() {
-		assertEquals("month: 5 day: 1", Main.nextDate(4, 30));
+	public void testCheckTriangle4() {
+		Main test = new Main();
+		assertEquals("是等腰三角形", test.checkTriangle(3, 3, 4));
 	}
 
 	@Test
-	public void testNextDate05() {
-		assertEquals("month: 7 day: 1", Main.nextDate(6, 30));
+	public void testCheckTriangle5() {
+		Main test = new Main();
+		assertEquals("三条边不能构成三角形", test.checkTriangle(3, -4, 4));
 	}
 
 	@Test
-	public void testNextDate06() {
-		assertEquals("month: 10 day: 1", Main.nextDate(9, 30));
+	public void testCheckTriangle6() {
+		Main test = new Main();
+		assertEquals("是等腰三角形", test.checkTriangle(3, 4, 3));
 	}
 
 	@Test
-	public void testNextDate07() {
-		assertEquals("month: 12 day: 1", Main.nextDate(11, 30));
+	public void testCheckTriangle7() {
+		Main test = new Main();
+		assertEquals("是等边三角形", test.checkTriangle(3, 3, 3));
 	}
 
 	@Test
-	public void testNextDate08() {
-		assertEquals("month: 1 day: 31", Main.nextDate(1, 30));
-	}
-
-	@Test
-	public void testNextDate09() {
-		assertEquals("month: 2 day: 1", Main.nextDate(1, 31));
-	}
-
-	@Test
-	public void testNextDate10() {
-		assertEquals("month: 4 day: 1", Main.nextDate(3, 31));
-	}
-
-	@Test
-	public void testNextDate11() {
-		assertEquals("month: 6 day: 1", Main.nextDate(5, 31));
-	}
-
-	@Test
-	public void testNextDate12() {
-		assertEquals("month: 8 day: 1", Main.nextDate(7, 31));
-	}
-
-	@Test
-	public void testNextDate13() {
-		assertEquals("month: 9 day: 1", Main.nextDate(8, 31));
-	}
-
-	@Test
-	public void testNextDate14() {
-		assertEquals("month: 11 day: 1", Main.nextDate(10, 31));
-	}
-
-	@Test
-	public void testNextDate15() {
-		assertEquals("month: 1 day: 1", Main.nextDate(12, 31));
-	}
-
-	@Test
-	public void testNextDate16() {
-		assertEquals("month: 11 day: 28", Main.nextDate(11, 27));
+	public void testCheckTriangle8() {
+		Main test = new Main();
+		assertEquals("三条边不能构成三角形", test.checkTriangle(1, 2, 4));
 	}
 }
