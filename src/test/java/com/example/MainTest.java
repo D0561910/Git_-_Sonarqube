5c902150a013440cfa36b7019a31b9be2e359d12
d0561910 <d0561910@mail.fcu.edu.tw>
2020-06-05 09:40:30 +0800

    Update MainTest.java
    v0.2

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -20,46 +20,4 @@ public class MainTest {
 		Main test = new Main();
 		assertEquals("是直角三角形", test.checkTriangle(3, 4, 5));
 	}
-
-	@Test
-	public void testCheckTriangle2() {
-		Main test = new Main();
-		assertEquals("三条边不能构成三角形", test.checkTriangle(-3, -4, -5));
-	}
-
-	@Test
-	public void testCheckTriangle3() {
-		Main test = new Main();
-		assertEquals("三条边不能构成三角形", test.checkTriangle(-5, 12, -13));
-	}
-
-	@Test
-	public void testCheckTriangle4() {
-		Main test = new Main();
-		assertEquals("是等腰三角形", test.checkTriangle(3, 3, 4));
-	}
-
-	@Test
-	public void testCheckTriangle5() {
-		Main test = new Main();
-		assertEquals("三条边不能构成三角形", test.checkTriangle(3, -4, 4));
-	}
-
-	@Test
-	public void testCheckTriangle6() {
-		Main test = new Main();
-		assertEquals("是等腰三角形", test.checkTriangle(3, 4, 3));
-	}
-
-	@Test
-	public void testCheckTriangle7() {
-		Main test = new Main();
-		assertEquals("是等边三角形", test.checkTriangle(3, 3, 3));
-	}
-
-	@Test
-	public void testCheckTriangle8() {
-		Main test = new Main();
-		assertEquals("三条边不能构成三角形", test.checkTriangle(1, 2, 4));
-	}
 }
